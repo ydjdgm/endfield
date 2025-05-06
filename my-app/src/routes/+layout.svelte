410902f5a6ecb1b3d33445692bfbc887900425c3
ydjdgm <tonytony060720@gmail.com>
2025-05-06 08:09:54 +0200

    .

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import "../app.css";
+
+	let { children } = $props();
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+	<a class="btn btn-ghost text-xl">daisyUI</a>
+</div>
+
+{@render children()}
