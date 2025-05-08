f9b2f64db5f48206e33cb074e5612e23d6cb6365
ydjdgm <tonytony060720@gmail.com>
2025-05-08 23:29:51 +0200

    Update +page.svelte
    드디어ㅓㅓ 캐릭터 id page 이정도면 만조쿠ㅡㅡㅡ는 무슨 이제 사진이랑 이름표만 붙였다는거임ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ아ㅋㅋㅋㅋ

--- my-app/src/routes/characters/[id]/+page.svelte
+++ my-app/src/routes/characters/[id]/+page.svelte
@@ -4,52 +4,60 @@
     import { Sparkle } from "@lucide/svelte";
 </script>
 
-<div class="w-full flex justify-center">
+<div class="w-full relative">
     <img
-        class="absolute z-0 pointer-events-none w-300 -mt-40"
-        src={`/${character.header_img}`}
+        src="/{character.header_img}"
         alt={character.name}
+        class="w-[50vw] pointer-events-none"
     />
-
     <div
-        class="relative mt-160 ml-140 w-[22rem] p-6 text-white bg-neutral-900 overflow-visible font-mono"
+        class="absolute p-4 w-50.5 right-1/2 top-[60vh] text-white bg-neutral-900 font-bold"
     >
         <!-- 고정된 사각형, 삐져나온 선들 (불규칙 길이) -->
         <!-- 상단선: 좌측 짧게 -->
         <div
             class="absolute top-0 left-[-12px] w-[calc(100%+24px)] h-[1px] bg-white"
         ></div>
-
         <!-- 하단선: 우측 길게 -->
         <div
-            class="absolute bottom-0 right-[-28px] w-[calc(100%+56px)] h-[1.5px] bg-white"
+            class="absolute bottom-0 right-[-28px] w-[calc(100%+56px)] h-[2px] bg-white"
         ></div>
-
         <!-- 좌측 세로선: 위쪽 짧게 -->
         <div
             class="absolute top-[-10px] left-0 h-[calc(100%+20px)] w-[1px] bg-white"
         ></div>
-
         <!-- 우측 세로선: 아래쪽 길게 -->
         <div
-            class="absolute bottom-[-18px] right-0 h-[calc(100%+36px)] w-[1.3px] bg-white"
+            class="absolute bottom-[-18px] right-0 h-[calc(100%+36px)] w-[2px] bg-white"
         ></div>
 
         <!-- 콘텐츠 -->
-        <div class="flex space-x-5">
-            <h1 class="text-2xl font-black tracking-tight mb-4">
+        <div class="flex flex-nowrap items-center space-x-4">
+            <h1 class="text-2xl font-black tracking-tight uppercase">
                 {character.name}
             </h1>
-            <div class="flex mt-1 space-x-1">
+            <div class="flex space-x-0.5">
                 {#each Array(character.rarity.id) as _, i}
-                    <Sparkle key={i} />
+                    <Sparkle key={i} class="w-4 h-4" />
                 {/each}
             </div>
         </div>
-        <div class="flex justify-between">
-            <img src={`/${character.element.img}`} class="" />
-            <img src={`/${character.class.img}`} class="" />
-            <img src={`/${character.weapon_type.img}`} class="" />
+        <div class="flex space-x-4 mt-2 justify-center">
+            <img
+                src={`/${character.element.img}`}
+                alt={character.element.name}
+                class=""
+            />
+            <img
+                src={`/${character.class.img}`}
+                alt={character.class.name}
+                class=""
+            />
+            <img
+                src={`/${character.weapon_type.img}`}
+                alt={character.weapon_type.name}
+                class=""
+            />
         </div>
     </div>
 </div>
