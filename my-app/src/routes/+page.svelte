39ebc8a147fe30e384660bd17c28b2a5938b406a
ydjdgm <tonytony060720@gmail.com>
2025-05-08 18:20:28 +0200

    .

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,2 @@
 <h1 class="font-bold text-4xl">Welcome to ENDFIELD INFO</h1>
-<a href="https://www.prydwen.gg/arknights-endfield/">pryden</a>
-<a href="https://ww.hakush.in/">hakush.in</a>
+<p>뜌땨뜌땨</p>
