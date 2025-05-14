933fdd1bb6e53681c4b3b4ef278ce78ff2f56030
ydjdgm <tonytony060720@gmail.com>
2025-05-14 15:29:55 +0200

    Update +page.svelte

--- my-app/src/routes/characters/[id]/+page.svelte
+++ my-app/src/routes/characters/[id]/+page.svelte
@@ -26,7 +26,7 @@
 <img src="/{character.header_img}" />
 
 <div class="fixed top-1/2 right-[1vw] -translate-y-1/2 z-50 flex flex-col space-y-2 p-3 bg-neutral-900/70 rounded-xl shadow-lg backdrop-blur-sm">
-	<button on:click={scrollToHeader} class="btn btn-md btn-neutral hover:btn-primary">Header</button>
-	<button on:click={scrollToStat} class="btn btn-md btn-neutral hover:btn-primary">Stat</button>
-	<button on:click={scrollToSkill} class="btn btn-md btn-neutral hover:btn-primary">Skill</button>
+	<button on:click={scrollToHeader} class="btn btn-md btn-neutral hover:btn-primary">해더</button>
+	<button on:click={scrollToStat} class="btn btn-md btn-neutral hover:btn-primary">기본 스탯</button>
+	<button on:click={scrollToSkill} class="btn btn-md btn-neutral hover:btn-primary">스킬</button>
 </div>
